6e0cb6aab1740c7d995b94a1939139de07f25874
John Leach <john.leach@intellitect.com>
2023-10-16 20:34:12 -0700

    Added firebase

---
+++ src/environments/environment.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  firebase: {
+    projectId: 'freedom-f7a50',
+    appId: '1:205838631958:web:dda9bb421e1a2feacce137',
+    storageBucket: 'freedom-f7a50.appspot.com',
+    locationId: 'us-central',
+    apiKey: 'AIzaSyBrwV-BACHWiI1GlK9HkHX3lvOB6ZnI7_0',
+    authDomain: 'freedom-f7a50.firebaseapp.com',
+    messagingSenderId: '205838631958',
+    measurementId: 'G-XBFXYS9CFR',
+  },
+};
